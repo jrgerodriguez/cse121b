9f4db2a2674f5c6d77c6a0393004c5c6423b5939
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-09-25 03:30:31 -0600

    final updates

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -5,7 +5,7 @@
 /* Step 2 - Variables */
 const fullName = "Jorge Alberto Menjivar Rodriguez";
 var currentYear = "2023";
-let profilePicture = "images/myprofilephoto.png";
+let profilePicture = "C:\Users\Admin\Desktop\byui\cse121b\images\myprofilephoto.png";
 
 
 
