379963820f55e68fe6de61c60d13af00600bf4f3
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-09-25 03:32:56 -0600

    final changes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -14,7 +14,7 @@ let profilePicture = "C:\Users\Admin\Desktop\byui\cse121b\images\myprofilephoto.
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector("img");
+const imageElement = document.querySelector('img');
 
 
 
@@ -22,8 +22,8 @@ const imageElement = document.querySelector("img");
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContext = currentYear;
-imageElement.setAttribute = ("src", profilePicture);
-imageElement.setAttribute = ("alt", `Profile image of ${fullName}`);
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 
 
