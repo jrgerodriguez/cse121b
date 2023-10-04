e1c5175fafd633225b471fc64110a9c8e35c8e5d
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-10-04 14:13:52 -0600

    Manipulating document complete

---
+++ sandbox/dom-basics.js
@@ -0,0 +1,19 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Image that was given");
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>CSE 121 b</h2><p>Welcome to Javascript Language</p>";
+document.body.appendChild(newSection);
+
+
+
