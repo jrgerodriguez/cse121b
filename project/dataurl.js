db6945706adb76796aa0f8526f463b2a31aa49f3
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-10-23 19:54:58 -0600

    project in progress

---
+++ project/dataurl.js
@@ -0,0 +1,3 @@
+var url = "https://run.mocky.io/v3/2814c17c-91f6-4ffe-bc58-bcef8476eeea";
+
+export default url;
--- project/project.js
+++ project/project.js
@@ -1,16 +1,21 @@
 //First I get the information from the JSON
+import url from './dataurl.js'
+
 const messageList = [];
 
 const getMessage = async () => {
-    const response = await fetch("https://run.mocky.io/v3/2814c17c-91f6-4ffe-bc58-bcef8476eeea");
+    const response = await fetch(url);
     const data = await response.json();
     messageList.push(...data);
     displayMessage(messageList);    
 };
 
-//I display the messages and create the categories.
+
+//Here, I create the categories first as option element which will append into the select element.
 const selectElement = document.getElementById('reason-list');
 const messageElement = document.getElementById('message-display');
+const calculatorOne = document.getElementById('first-calculator');
+const calculatorTwo = document.getElementById('second-calculator');
 
 const displayMessage = (categories) => {
     categories.forEach(category => {
@@ -19,18 +24,40 @@ const displayMessage = (categories) => {
         optionElement.setAttribute("value", category.reason);
         selectElement.appendChild(optionElement);
 
+
+// The next event will pick the message based on the selected option.
         selectElement.addEventListener('change', function()
         {
-            const selectedReason = selectElement.value;
-            const selectedCategory = categories.find(category => category.reason === selectedReason);
+            const selectedOption = selectElement.value;
+            const selectedCategory = categories.find(category => category.reason === selectedOption);
     
         if (selectedCategory)
         {
             messageElement.textContent = `IMPORTANT: ${selectedCategory.percentage}`;
-        }   
-    })})};
+        }
 
-displayMessage(messageList);
+
+//This last event will display the calculator that will provide with the correct result based on the chosen option.
+
+        calculatorOne.style.display = 'none';
+        calculatorTwo.style.display = 'none';
+
+
+        if (selectedCategory.reason === categories[0].reason || selectedCategory.reason === categories[1].reason)
+        {
+            calculatorOne.style.display = 'block';
+            
+        }
+
+        else if (selectedCategory.reason === categories[2].reason)
+        {
+            calculatorTwo.style.display = 'block';
+        }     
+
+    });
+})};
+
+
 getMessage()
 
 
@@ -38,7 +65,7 @@ getMessage()
 
 
 //I create the function for the first calculator
-function hundredPercent(number1, number2)
+function hundredPercent(number1, number2) 
 {
     return number1 * number2;
 }
@@ -48,11 +75,11 @@ function firstCalculator()
 {
     let numberOne = Number(document.querySelector('#salary').value);
     let numberTwo = Number(document.querySelector('#years').value);    
-    document.querySelector('#result-first-calculator').value = hundredPercent(numberOne, numberTwo);    
+    document.querySelector('#result-first-calculator').value = `You will be paid $${hundredPercent(numberOne, numberTwo)}`;
 }
 
 
-function showAlertOne()
+function showAlertOne()//An Alert is included to enter a number no lower than 2 representing the amount of worked years
 {
     var value = 
     parseFloat(document.querySelector('#years').value);
@@ -64,8 +91,10 @@ function showAlertOne()
 
 }
 
+
 document.querySelector('#get-first-result').addEventListener('click', firstCalculator);
 document.querySelector('#years').addEventListener('input', showAlertOne);
+
 
 //I create the function for the second calculator
 function halfPerYear (number1, number2)
@@ -78,37 +107,89 @@ function secondCalculator()
 {
     let numberOne = Number(document.querySelector('#salary2').value);
     let numberTwo = Number(document.querySelector('#years2').value);    
-    document.querySelector('#result-second-calculator').value = halfPerYear(numberOne, numberTwo);    
+    document.querySelector('#result-second-calculator').value = `You will be paid $${halfPerYear(numberOne, numberTwo)}`;    
 }
 
-function showAlertTwo()
+
+function showAlertTwo() //An Alert is included to enter a number no lower than 2 representing the amount of worked years
 {
     var value = 
     parseFloat(document.querySelector('#years2').value);
     if (value < 2) 
     {
         alert("REMEMBER: If you have been working for less than two years, the company is not obligated to pay a compensation since you are not elegible for this benefit.");
-        document.querySelector('#years').value = "";
+        document.querySelector('#years2').value = "";
     } 
 
 }
 
+
 document.querySelector('#get-second-result').addEventListener('click', secondCalculator);
 document.querySelector('#years2').addEventListener('input', showAlertTwo);
 
+
 //I create the function for the average calculator and put it all in an array
-//I will sum everything that is stored and get a total. 
+//The array uses the reduce() method to get the average of the numbers it has stored.
 
-const salaries = [];
+function aveTotal()
+{
+    var salaries = [];
+    
 
-const firstMonth = document.querySelector('#first-month').value;
-salaries.push(firstMonth);
+    for (var i = 1; i < 2; i++)
+    {
+        var inputElement1 = document.querySelector('#first-month');
+        var inputElement2 = document.querySelector('#second-month'); 
+        var inputElement3 = document.querySelector('#third-month');    
+        var inputElement4 = document.querySelector('#fourth-month');    
+        var inputElement5 = document.querySelector('#fifth-month');    
+        var inputElement6 = document.querySelector('#sixth-month');
 
+        var valueInput1 = parseFloat(inputElement1.value);
+        var valueInput2 = parseFloat(inputElement2.value);
+        var valueInput3 = parseFloat(inputElement3.value);
+        var valueInput4 = parseFloat(inputElement4.value);
+        var valueInput5 = parseFloat(inputElement5.value);
+        var valueInput6 = parseFloat(inputElement6.value);
 
+        if (!isNaN(valueInput1))
+        {
+            salaries.push(valueInput1);
+        }
+        if (!isNaN(valueInput2))
+        {
+            salaries.push(valueInput2);
+        }
+        if (!isNaN(valueInput3))
+        {
+            salaries.push(valueInput3);
+        }
+        if (!isNaN(valueInput4))
+        {
+            salaries.push(valueInput4);
+        }
+        if (!isNaN(valueInput5))
+        {
+            salaries.push(valueInput5);
+        }
+        if (!isNaN(valueInput6))
+        {
+            salaries.push(valueInput6);
+        }    
+    }
 
+    var ave = salaries.reduce(function(a, b)
+    {
+        return a + b
+        
+    }, 0) / salaries.length; 
 
+    document.getElementById('average').value = `Your salary average is $${ave.toFixed(2)}`;
 
+    
+}
 
+const averageButton = document.getElementById('button-average').addEventListener('click', aveTotal);
 
 
 
